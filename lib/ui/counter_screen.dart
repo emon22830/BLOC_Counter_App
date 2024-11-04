ddaf808993b0e130145753b9b28ade9ecd4e0ed3
emon22830 <mdemon22830@gmail.com>
2024-11-04 22:30:18 +0800

    Completed UI Design

---
+++ lib/ui/counter_screen.dart
@@ -0,0 +1,40 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text('Couter App'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('0', style: TextStyle(fontSize: 60),)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              ElevatedButton(onPressed: (){}, child: Text('Increment')),
+              SizedBox(width: 20,),
+              ElevatedButton(onPressed: (){}, child: Text('Decrement')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
